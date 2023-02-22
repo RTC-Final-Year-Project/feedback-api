2bacfccf1afb0cb6e53e5b99a47855f5e2342ed8
Rehannah <rehannah12345@gmail.com>
2023-02-22 02:15:03 +0000

    patched up errors with rules

--- feedback_module/spelling_rules/Rule_Q_Followed_By_U.py
+++ feedback_module/spelling_rules/Rule_Q_Followed_By_U.py
@@ -2,14 +2,15 @@ import re
 
 from .Rule import Rule
 
+
 class Rule_Q_Followed_By_U(Rule):
   id = 15
   rule = "The letter 'q' is usually followed by the letter 'u'"
   examples = "queen, earthquake, quick"
   exceptions = "Qatar, niqab"
-  
+
   @staticmethod
   def check_if_followed(actual_word, attempted_word):
-    if re.search("qu", actual_word, re.I) and not re.search("qu", attempted_word, re.I):
+    if re.search("qu", actual_word, re.I) and (re.search("q|u", attempted_word, re.I) and not re.search("qu", attempted_word, re.I)):
       return False
     return True
