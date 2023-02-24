3d28d25161c5fa7737dbfd8099e020653b6f5f84
Rehannah <rehannah12345@gmail.com>
2023-02-24 19:34:17 -0400

    rules v or j and dge --> dge test fails

--- tests/StandardRuleTests/test_Rule_Ending_In_D_G_E.py
+++ tests/StandardRuleTests/test_Rule_Ending_In_D_G_E.py
@@ -1,37 +1,35 @@
 from ..AssessTestCases import AssessTestCases
 
-from feedback_module.spelling_rules import Rule_Ending_In_V_Or_J
+from feedback_module.spelling_rules import Rule_Ending_In_D_G_E
 
 
-rule = Rule_Ending_In_V_Or_J
+rule = Rule_Ending_In_D_G_E
 
 # the list of combinations that are supposed to follow this rule (thus being ignored by the module checker because isFollowed = True)
 combinations_that_follow_rule = combinations_ignore_rule = [
     ["hello", "world"],
-    ["edge", "edge"],
     ["judge", "judge"],
-    ["rummage", "rummage"],
-    ["hive", "hive"],
-    ["live", "live"],
-    ["positive", "positive"],
+    ["edge", "edge"],
+    ["porridge", "porridge"],
+    ["rummage", "rummidge"],
+    ["fridge", "fridge"],
 ]
 
 # the list of combinations that are supposed to violate this rule (thus being detected by the module checker because isFollowed = False)
 combinations_that_violate_rule = combinations_detect_rule = [
-    ["hive", "hyv"],
     ["judge", "juj"],
-    ["hedge", "hej"],
-    ["live", "liv"],
-    ["rummage", "rumej"],
-    ["positive", "positiv"],
+    ["judge", "jage"],
+    ["judge", "juge"],
+    ["judge", "jug"],
+    ["judge", "judg"],
 ]
 
-class Rule_Ending_In_V_Or_J_Unit_Tests(AssessTestCases.UnitTestCases):
+class Rule_Ending_In_D_G_E_Unit_Tests(AssessTestCases.UnitTestCases):
     rule = rule
     combinations_that_follow_rule = combinations_that_follow_rule
     combinations_that_violate_rule = combinations_that_violate_rule
 
-class Rule_Ending_In_V_Or_J_Integration_Tests(AssessTestCases.IntegrationTestCases):
+class Rule_Ending_In_D_G_E_Integration_Tests(AssessTestCases.IntegrationTestCases):
     rule = rule
     combinations_ignore_rule = combinations_ignore_rule
     combinations_detect_rule = combinations_detect_rule
