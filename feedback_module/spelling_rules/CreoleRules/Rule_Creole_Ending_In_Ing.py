97f3d22665d87b9bf3b0241684adb21670566f28
Rehannah <rehannah12345@gmail.com>
2023-02-26 15:27:38 -0400

    updates to ing rule and tests

--- feedback_module/spelling_rules/CreoleRules/Rule_Creole_Ending_In_Ing.py
+++ feedback_module/spelling_rules/CreoleRules/Rule_Creole_Ending_In_Ing.py
@@ -7,6 +7,6 @@ class Rule_Creole_Ending_In_Ing(CreoleRule):
   
   @staticmethod
   def check_if_followed(actual_word, attempted_word):
-    if (re.search("(in|n$)", attempted_word, re.I) and actual_word.endswith("ing")):
+    if (re.search("(in|n)$", attempted_word, re.I) and actual_word.endswith("ing")):
       return False
     return True
---
+++ tests/CreoleRuleTests/test_Creole_Rule_Ending_In_Ing.py
@@ -0,0 +1,33 @@
+from ..AssessTestCases import AssessTestCases
+
+from feedback_module.spelling_rules import Rule_Creole_Ending_In_Ing
+
+rule = Rule_Creole_Ending_In_Ing
+
+# the list of combinations that are supposed to follow this rule (thus being ignored by the module checker because isFollowed = True)
+combinations_that_follow_rule = combinations_ignore_rule = [
+    ["hello", "world"],
+    ["duck", "dock"],
+    ["asking", "asking"],
+    ["falling", "falling"],
+    ["telling", "telling"],
+]
+
+# the list of combinations that are supposed to violate this rule (thus being detected by the module checker because isFollowed = False)
+combinations_that_violate_rule = combinations_detect_rule = [
+    ["asking", "askin"],
+    ["falling", "fallen"],
+    ["falling", "fallin"],
+    ["falling", "falln"],
+    ["telling", "tellin"],
+]
+
+class Rule_Creole_Ending_In_Ing_Unit_Tests(AssessTestCases.UnitTestCases):
+    rule = rule
+    combinations_that_follow_rule = combinations_that_follow_rule
+    combinations_that_violate_rule = combinations_that_violate_rule
+
+class Rule_Creole_Ending_In_Ing_Integration_Tests(AssessTestCases.IntegrationTestCases):
+    rule = rule
+    combinations_ignore_rule = combinations_ignore_rule
+    combinations_detect_rule = combinations_detect_rule
