dde6a17090c68efea95ab1dd9d537b5347bfc237
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2023-03-05 03:10:14 +0000

    Fixed error in Rule_Plural_By_Change_Y_To_I_Add_Es

--- feedback_module/spelling_rules/PluralRules/__init__.py
+++ feedback_module/spelling_rules/PluralRules/__init__.py
@@ -1,11 +1,11 @@
 from .Rule_Plural_By_Adding_S import Rule_Plural_By_Adding_S
 from .Rule_Plural_Ch_Sh_Z_X_S_O_is_Es import Rule_Plural_Ch_Sh_Z_X_S_O_is_Es
 from .Rule_Plural_F_Or_Fe_To_Ves import Rule_Plural_F_Or_Fe_To_Ves
-# from .Rule_Plural_By_Change_Y_To_I_Add_Es import Rule_Plural_By_Change_Y_To_I_Add_Es
+from .Rule_Plural_By_Change_Y_To_I_Add_Es import Rule_Plural_By_Change_Y_To_I_Add_Es
 
 plural_rules_list = [
     Rule_Plural_By_Adding_S,
     Rule_Plural_Ch_Sh_Z_X_S_O_is_Es,
     Rule_Plural_F_Or_Fe_To_Ves,
-    # Rule_Plural_By_Change_Y_To_I_Add_Es,
+    Rule_Plural_By_Change_Y_To_I_Add_Es,
 ]
--- tests/PluralRuleTests/test_Rule_Plural_By_Change_Y_To_I_Add_Es.py
+++ tests/PluralRuleTests/test_Rule_Plural_By_Change_Y_To_I_Add_Es.py
@@ -1,29 +1,29 @@
-# from ..AssessTestCases import AssessTestCases
+from ..AssessTestCases import AssessTestCases
 
-# from feedback_module.spelling_rules import Rule_Plural_By_Change_Y_To_I_Add_Es
+from feedback_module.spelling_rules import Rule_Plural_By_Change_Y_To_I_Add_Es
 
-# rule = Rule_Plural_By_Change_Y_To_I_Add_Es
+rule = Rule_Plural_By_Change_Y_To_I_Add_Es
 
-# combinations_that_follow_rule = combinations_ignore_rule = [
-#     ["cities", "cities"],
-#     ["parties", "parties"],
-#     ["babies", "babies"],
-# ]
+combinations_that_follow_rule = combinations_ignore_rule = [
+    ["cities", "cities"],
+    ["parties", "parties"],
+    ["babies", "babies"],
+]
 
-# # the list of combinations that are supposed to violate this rule (thus being detected by the module checker because isFollowed = False)
-# combinations_that_violate_rule = combinations_detect_rule = [
-#     ["cities", "citys"],
-#     ["parties", "partys"],
-# ]
+# the list of combinations that are supposed to violate this rule (thus being detected by the module checker because isFollowed = False)
+combinations_that_violate_rule = combinations_detect_rule = [
+    ["cities", "citys"],
+    ["parties", "partys"],
+]
 
 
 
-# class Rule_Plural_By_Change_Y_To_I_Add_Es_Unit_Tests(AssessTestCases.UnitTestCases):
-#     rule = rule
-#     combinations_that_follow_rule = combinations_that_follow_rule
-#     combinations_that_violate_rule = combinations_that_violate_rule
+class Rule_Plural_By_Change_Y_To_I_Add_Es_Unit_Tests(AssessTestCases.UnitTestCases):
+    rule = rule
+    combinations_that_follow_rule = combinations_that_follow_rule
+    combinations_that_violate_rule = combinations_that_violate_rule
 
-# class Rule_Plural_By_Change_Y_To_I_Add_Es_Integration_Tests(AssessTestCases.IntegrationTestCases):
-#     rule = rule
-#     combinations_ignore_rule = combinations_ignore_rule
-#     combinations_detect_rule = combinations_detect_rule
+class Rule_Plural_By_Change_Y_To_I_Add_Es_Integration_Tests(AssessTestCases.IntegrationTestCases):
+    rule = rule
+    combinations_ignore_rule = combinations_ignore_rule
+    combinations_detect_rule = combinations_detect_rule
