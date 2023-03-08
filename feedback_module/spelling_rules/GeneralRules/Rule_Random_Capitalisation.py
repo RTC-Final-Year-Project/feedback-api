90e7bfce9440ee1162d233603696f3dd236cde34
Rehannah <rehannah12345@gmail.com>
2023-03-08 12:26:54 -0400

    random cap updated

--- feedback_module/spelling_rules/GeneralRules/Rule_Random_Capitalisation.py
+++ feedback_module/spelling_rules/GeneralRules/Rule_Random_Capitalisation.py
@@ -6,7 +6,9 @@ class Rule_Random_Capitalisation(GeneralRule):
   
   @staticmethod
   def check_if_followed(actual_word, attempted_word):
-    for i in range(1, len(attempted_word)):
+    # length check - to ensure case matches even if number of letters in the words differ
+    length=min(len(attempted_word), len(actual_word))
+    for i in range(1, length):
       if (actual_word[i].isupper() and not attempted_word[i].isupper()) or (actual_word[i].islower() and not attempted_word[i].islower()):
         return False
     return True
